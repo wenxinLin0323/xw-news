b61a2c9f5405958d1dd1fe4fb4def1a0837de3c4
WenxinLin <2358634856@qq.com>
2021-04-27 22:14:36 +0800

    初始准备

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-
+// 导入通用的样式
+import './styles/common.less'
+// 导入字体图标库
+import './styles/iconfont.less'
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount('#app')
